5e45227777c52b6727b4f3866a39c924a94492de
Clyff Millet <clyffm@gmail.com>
2017-04-01 02:23:48 -0400

    New Category VC uiimagePickerDelegate methods set

--- ShelfLife/NewCategoryViewController.swift
+++ ShelfLife/NewCategoryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NewCategoryViewController: UIViewController {
+class NewCategoryViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
     @IBOutlet var categoryImageView: UIImageView!
@@ -16,6 +16,7 @@ class NewCategoryViewController: UIViewController {
     
     
     
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,10 +29,49 @@ class NewCategoryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Actions
+
     @IBAction func addImage(_ sender: Any) {
+        
+        // Privacy settings in plist have been set for camera
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // allow photos to taken.
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera))
+        {
+            imagePickerController.sourceType = .camera
+        }
+        else
+        {
+            imagePickerController.sourceType = .photoLibrary
+        }
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
     }
 
     @IBAction func saveButtonPressed(_ sender: Any) {
+    }
+    
+    // MARK: - UIImage Picker Controller Delegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        categoryImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
     }
     
     /*
