ca61f5fa1bfaf6b0476e7c733d7d29d5686dc000
Clyff Millet <clyffm@gmail.com>
2017-04-01 02:15:51 -0400

    new category vc UI added

---
+++ ShelfLife/NewCategoryViewController.swift
@@ -0,0 +1,47 @@
+//
+//  NewCategoryViewController.swift
+//  ShelfLife
+//
+//  Created by Clyfford Millet on 4/1/17.
+//  Copyright © 2017 Clyff Millet. All rights reserved.
+//
+
+import UIKit
+
+class NewCategoryViewController: UIViewController {
+    
+    
+    @IBOutlet var categoryImageView: UIImageView!
+    @IBOutlet var nameTextField: UITextField!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addImage(_ sender: Any) {
+    }
+
+    @IBAction func saveButtonPressed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
